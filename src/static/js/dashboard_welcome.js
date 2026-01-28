cf0eaadc7c46e12b133102c611499bb9f9b3117f
k0f1 <kofuafor@gmail.com>
2026-01-28 16:38:15 +0000

    Switched dashboard submenu default to impact

--- src/static/js/dashboard_welcome.js
+++ src/static/js/dashboard_welcome.js
@@ -304,7 +304,7 @@
   }
 
   if (dashboardTabs.length && dashboardPanels.length) {
-    const defaultTab = 'connections';
+    const defaultTab = 'impact';
     const initial = dashboardTabs.find((btn) => btn.dataset.dashboardSection === defaultTab);
     if (initial) setActiveDashboardTab(defaultTab);
     dashboardTabs.forEach((btn) => {
