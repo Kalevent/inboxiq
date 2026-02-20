c92f2f11e998c09db5fd3af7e8d5d5e9ef0cabf7
k0f1 <kofuafor@gmail.com>
2026-02-20 17:23:26 +0000

    Update social_auth route.py

--- src/social_auth/routes.py
+++ src/social_auth/routes.py
@@ -33,7 +33,7 @@ from src.social_auth import bp
 _AUTH_URL = "https://www.linkedin.com/oauth/v2/authorization"
 _TOKEN_URL = "https://www.linkedin.com/oauth/v2/accessToken"
 _USERINFO_URL = "https://api.linkedin.com/v2/userinfo"
-_SCOPE = "w_member_social r_liteprofile openid"
+_SCOPE = "openid profile email w_member_social"
 _PROVIDER = "linkedin_social"
 
 
